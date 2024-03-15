970f5a3e0b6c6ab88824d9d4420bb0b1aebdfec7
dianprsty <dprasmail@gmail.com>
2024-03-15 16:23:58 +0700

    feat: landing page

--- src/inoobwebforum/urls.py
+++ src/inoobwebforum/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include("landing.urls")),
     path('auth/', include("django.contrib.auth.urls")),
     path('admin/', admin.site.urls),
 ]
