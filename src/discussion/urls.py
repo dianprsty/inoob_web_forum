339a235611063e100dd835b87750d741ec125fdc
dianprsty <dprasmail@gmail.com>
2024-03-20 00:13:21 +0700

    feat: my QnA

--- src/discussion/urls.py
+++ src/discussion/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import dashboard_views
+from .views import dashboard_views, my_qna_view
 
 urlpatterns = [
     path("dashboard/", dashboard_views.dashboard, name="dashboard"),
+    path("myqna/", my_qna_view.my_qna, name="myqna"),
     # view discussion bisa diubah ke custom view mas satya
     path('discussion/detail/<int:id>', dashboard_views.dashboard, name='detail'),
 
