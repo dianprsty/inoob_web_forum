c03806cc192bb6490f3894a90d5cd4f49c7b9ac8
dianprsty <dprasmail@gmail.com>
2024-03-19 15:58:34 +0700

    refactor: refactor ui dashboard

--- src/discussion/views/dashboard_views.py
+++ src/discussion/views/dashboard_views.py
@@ -1,20 +1,25 @@
 from django.db import connection
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from .helper import dictfetchall
+from .helpers import cursor_to_dict, date_to_time_ago
+
 
 # Create your views here.
-
 
 def get_question():
     with connection.cursor() as cursor:
         cursor.execute("""
-            SELECT * FROM questions;
+            SELECT title, content, questions.created_at as created_at, questions.id,
+                username
+            FROM questions
+            JOIN auth_user
+            ON questions.user_id = auth_user.id;
                        """)
-        row = dictfetchall(cursor)
+        row = cursor_to_dict.dictfetchall(cursor)
     return row
 
 @login_required
 def dashboard(request, id=1):
     questions = get_question()
-    return render(request, "discussion/dashboard.html", {"questions":questions})
+    time_ago = date_to_time_ago.to_time_ago(questions[0]["created_at"])
+    return render(request, "discussion/dashboard.html", {"questions":questions, "time": time_ago})
---
+++ src/discussion/views/helpers/date_to_time_ago.py
@@ -0,0 +1,33 @@
+from django.utils import timezone
+
+def to_time_ago(date):
+    time_now = timezone.now()
+    time_created = timezone.make_aware(date)
+    time_difference = time_now - time_created
+
+    seconds =   time_difference.seconds
+    minutes = seconds // 60
+    hours = minutes // 60
+    days = hours // 24
+    weeks =  days // 7
+    months = days // 30
+    years =  days // 365
+
+    time_value = 0
+    time_unit = ""
+    if years > 0 :
+        time_value, time_unit = years, "year"
+    elif months > 0 :
+        time_value, time_unit = months, "month"
+    elif weeks > 0 :
+        time_value, time_unit = weeks, "week"
+    elif days > 0 :
+        time_value, time_unit = days, "day"
+    elif hours > 0 :
+        time_value, time_unit = hours, "hour"
+    elif minutes > 0 :
+        time_value, time_unit = minutes, "minute"
+    else :
+        time_value, time_unit = seconds, "second"
+    time_plural = "s" if time_value > 1 else ""
+    return f"{time_value} {time_unit + time_plural} ago"
