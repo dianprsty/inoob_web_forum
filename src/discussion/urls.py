acfceb70aa5bca1fdd74a2a85e328075f19b1e2b
dianprsty <dprasmail@gmail.com>
2024-03-22 22:04:53 +0700

    feat: profile

--- src/discussion/urls.py
+++ src/discussion/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import dashboard_views, my_qna_view, saved_view
+from .views import dashboard_views, my_qna_view, saved_view, profile_view
 
 urlpatterns = [
     path("dashboard/", dashboard_views.dashboard, name="dashboard"),
     path("myqna/", my_qna_view.my_qna, name="myqna"),
     path("saved/", saved_view.saved, name="saved"),
+    path("profile/", profile_view.profile, name="profile"),
     # view discussion bisa diubah ke custom view mas satya
     path('discussion/detail/<int:id>', dashboard_views.dashboard, name='detail'),
 
