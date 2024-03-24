9e1d13f3ef75ade93c4c5bb9eb06b95eee6a364c
dianprsty <dprasmail@gmail.com>
2024-03-24 08:59:15 +0700

    fix: profile

--- src/authentication/views.py
+++ src/authentication/views.py
@@ -1,14 +1,23 @@
+from django.db import connection
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 
-
+def create_profile(user_id):
+     with connection.cursor() as cursor:
+        cursor.execute("""
+            INSERT INTO profiles
+            (user_id) VALUES (%s)
+                       """, [user_id])
 # Create your views here.
 def register(response):
     if response.method == "POST":
         form = RegisterForm(response.POST)
         if form.is_valid():
-            form.save()
-            return redirect("landing")
+            user = form.save()
+            user_id =   user.id
+            create_profile(user_id)
+
+        return redirect("login")
     else:
 	    form = RegisterForm()
-    return render(response, "registration/register.html", {"form":form})
+    return render(response, "registration/register.html", {"form": form })
